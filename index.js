aa559668a0546400094bb17dc08f6c474f415ffb
brave-ping-4201 <h_yazie@yahoo.com>
2020-01-24 08:47:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ var katzDeliLine = [];
 //function returns welcome message
 
 function takeANumber(katzDeli, name) {
-  katzDeli.push(`${name}`);
+  katzDeli.push(name);
   return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
 }
+
+
 //building a function now serving
-function nowServing(katzDeliLine){if (katzDeliLine.length > 0){return "Currently serving" + katzDeliLine.shift()+ ".";}
-else { return "There is nobody waiting to be served!";}}
+function nowServing(katzDeliLine){
+  
+  if (katzDeliLine.length > 0){
+    
+    return "Currently serving " + katzDeliLine.shift()+ ".";
+    
+  }else{
+    return "There is nobody waiting to be served!";}}
 
 
-function nowServing(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "There is nobody waiting to be served!";
-  }
-  else
-  return (`Currently serving ${katzDeli.shift()}.`);
-}
+
+
+
+
 
 function currentLine(katzDeliLine){
   let line = ""
@@ -35,6 +36,7 @@ function currentLine(katzDeliLine){
   for (let i =0; i < katzDeliLine.length; i++) {
   
 line += `${i+1}. ${katzDeliLine[i]}, `;
+     
      
   }
      line = line.slice(0, line.length - 2);
